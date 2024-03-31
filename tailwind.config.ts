951c5ccb981c76ea493833e5bb78b7accb928339
Lee <leesheppard2404@gmail.com>
2024-03-31 20:31:13 +0200

    Update tailwind.config.ts with new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: '#CED7DA',
+        primary: '#EBF0F2',
+        secondary: '#6F90DE',
+        secondaryDark: '#3F6BD3',
+        secondaryDarker: '#2F509E',
+        tertiary: '#49AB98',
+        accent: '#515D86',
+        dark: '#212735',
       },
     },
   },
