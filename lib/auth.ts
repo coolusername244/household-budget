408b01c2a42a1d4546beee4c8d26ded3f5c71348
Lee <leesheppard2404@gmail.com>
2024-04-04 21:03:27 +0200

    Add authentication route and auth options

---
+++ lib/auth.ts
@@ -0,0 +1,81 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { prisma } from './db';
+import { compare } from 'bcrypt';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'johndoe@example.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const existingUser = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const isValid = await compare(
+          credentials.password,
+          existingUser.password,
+        );
+
+        if (!isValid) {
+          return null;
+        }
+
+        return {
+          id: existingUser.id,
+          email: existingUser.email,
+          firstName: existingUser.firstName,
+          lastName: existingUser.lastName,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          firstName: token.firstName,
+          lastName: token.lastName,
+        },
+      };
+    },
+  },
+};
