4d521b6eb1dd8f6a70fd609292c11ec0a73b4a70
Lee <leesheppard2404@gmail.com>
2024-04-04 21:01:57 +0200

    added route page template

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = async () => {
+  const session = await getServerSession(authOptions);
+  return <main>Hello there {session?.user.email}</main>;
 };
 
 export default Dashboard;
