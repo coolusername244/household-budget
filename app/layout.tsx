baab533da729ef2ef6264185803f1d5248a512b8
Lee <leesheppard2404@gmail.com>
2024-04-04 21:01:24 +0200

    Add Provider component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import Provider from './_components/Provider';
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800'],
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
