dd4f976778d5f3f6c1f672aa86b5418c9d75b729
Lee <leesheppard2404@gmail.com>
2024-04-05 11:58:10 +0200

    Refactor DashboardNavbar component to display dynamic username

--- app/_components/Navbar/DashboardNavbar.tsx
+++ app/_components/Navbar/DashboardNavbar.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import { getServerSession } from 'next-auth';
 import { dashboardLinks } from './constants';
 import Navlink from '@/app/_components/Navbar/Navlink';
-import Link from 'next/link';
 import SignOutButton from '@/Buttons/SignOutButton';
+import { authOptions } from '@/lib/auth';
 
 const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+  const { firstName, email } = session!.user;
+  const username = firstName || email!.split('@')[0];
+
   return (
     <nav className="dashboard-nav">
-      <h1 className="">Welcome, Username!</h1>
+      <h1 className="">Welcome, {username}!</h1>
       <ul className="flex-1 pt-6">
         {dashboardLinks.map((link, i) => (
           <li key={i} className="pt-6">
