8d3ac89413870cd8e52a3bd82658a1bf21632df9
Lee <leesheppard2404@gmail.com>
2024-04-14 12:29:11 +0200

    Refactor Navlink component to include active class

--- app/_components/Navbar/Navlink.tsx
+++ app/_components/Navbar/Navlink.tsx
@@ -1,10 +1,19 @@
+'use client';
 import React, { FC } from 'react';
 import Link from 'next/link';
 import { NavlinkT } from '@/types/Navlink.type';
+import { usePathname } from 'next/navigation';
 
 const Navlink: FC<NavlinkT> = ({ className, href, text }) => {
+  const pathname = usePathname();
+
   return (
-    <Link className={className} href={href}>
+    <Link
+      className={`${className} ${
+        pathname === `/${text.toLowerCase()}` ? 'text-white' : ''
+      }`}
+      href={href}
+    >
       <span className="flex-1">{text}</span>
     </Link>
   );
